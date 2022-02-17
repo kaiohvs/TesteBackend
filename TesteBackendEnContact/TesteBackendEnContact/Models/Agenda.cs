6651bc5b480411a67f09d18bf57e83ae9d573625
kaio <kaiohvs@gmail.com>
2022-02-17 18:25:59 -0300

    Criacao do Models e configuracao do context no model e no startup

---
+++ TesteBackendEnContact/TesteBackendEnContact/Models/Agenda.cs
@@ -0,0 +1,10 @@
+﻿namespace TesteBackendEnContact.Models
+{
+    public class Agenda
+    {
+        public int Id { get; set; }
+        public int Telefone { get; set; }
+        public string Nome { get; set; }
+
+    }
+}
---
+++ TesteBackendEnContact/TesteBackendEnContact/Models/AgendaContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TesteBackendEnContact.Models
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext( DbContextOptions<AgendaContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Agenda> Agendas { get; set; }
+    }
+}
